d86caf3a27943cb24df690cc3fd0cd4b2aa499b1
huyhoang8398 <huyhoang8398@huyhoang8398s-MacBook-Pro.local>
2018-05-23 12:29:39 +0700

    Monitoring a filesystem with Python and Pyinotify

---
+++ pyinotify/inotify.py
@@ -0,0 +1,17 @@
+import pyinotify
+from datetime import datetime
+wm = pyinotify.WatchManager()  # Watch Manager
+mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE  # watched events
+
+class EventHandler(pyinotify.ProcessEvent):
+    def process_IN_CREATE(self, event):
+        print "Creating:", event.pathname,str(datetime.now())
+
+    def process_IN_DELETE(self, event):
+        print "Removing:", event.pathname
+
+handler = EventHandler()
+notifier = pyinotify.Notifier(wm, handler)
+wdd = wm.add_watch('/home', mask, rec=True)
+
+notifier.loop()
