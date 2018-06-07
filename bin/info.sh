5e1b9f8510cb8b678f157e2eb822cfa72774b54d
huyhoang8398 <huyhoang8398@huyhoang8398s-MacBook-Pro.local>
2018-06-07 19:13:28 +0700

    Script for everything

--- bin/info.sh
+++ bin/info.sh
@@ -9,6 +9,10 @@ notiF=$(find /Users/huyhoang8398/Desktop/scann -mtime -1 -ls)
 infoPng=$(find /Users/huyhoang8398/Pictures | grep .png | wc -l)
 infoJpg=$(find /Users/huyhoang8398/Pictures | grep .jpg | wc -l)
 
+#Read data from pyinotify log file. 
+pyInoC=$(fgrep 'CREATED' ~/IOT-USTH/test.txt)
+pyInoD=$(fgrep 'DELETED' ~/IOT/USTH/test.txt)
+
 function GET_MEM()
 {
     echo -e "$str3 $avaiMem\n";  
@@ -29,6 +33,17 @@ function GET_JPG()
     echo -e "$str1 $infoJpg\n";
 }
 
+function GET_WRITED()
+{
+    echo -e "$pyInoC\n";
+}
+
+function GET_DELETED()
+{
+    echo -e "$pyInoD\n";
+}
+
+
 # Write date, time > overwrite all files data
 date > /Users/huyhoang8398/test.txt
 
@@ -40,6 +55,7 @@ echo $(GET_JPG) >> /Users/huyhoang8398/test.txt
 echo $(GET_PNG) >> /Users/huyhoang8398/test.txt
 echo $(GET_MEM) >> /Users/huyhoang8398/test.txt
 echo $(GET_WR) >> /Users/huyhoang8398/test.txt
-
+echo $(GET_WRITED) >> /Users/huyhoang8398/test.txt
+echo $(GET_DELETED) >> /Users/huyhoang8398/test.txt
 
 
