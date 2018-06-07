0a3d823e28af987423d4658cc7fbc9216675757e
huyhoang8398 <huyhoang8398@huyhoang8398s-MacBook-Pro.local>
2018-06-07 11:52:34 +0700

    Script files > Python

--- bin/info.sh
+++ bin/info.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+str1="Images JPG:"
+str2="Images PNG:"
 date > /Users/huyhoang8398/test.txt
-printf "`echo Images JPG:` `find /Users/huyhoang8398/Pictures | grep .jpg | wc -l`\n" >> /Users/huyhoang8398/test.txt
-printf "`echo Images PNG:` `find /Users/huyhoang8398/Pictures | grep .png | wc -l`" >> /Users/huyhoang8398/test.txt
+printf "`echo $str1` `find /Users/huyhoang8398/Pictures | grep .jpg | wc -l`\n" >> /Users/huyhoang8398/test.txt
+printf "`echo $str2` `find /Users/huyhoang8398/Pictures | grep .png | wc -l`" >> /Users/huyhoang8398/test.txt
