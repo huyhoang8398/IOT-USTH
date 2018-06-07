65c67fc4b0de817481f56833d9fabaa7c78721d9
huyhoang8398 <huyhoang8398@huyhoang8398s-MacBook-Pro.local>
2018-06-07 14:58:19 +0700

    Script files > Python

--- bin/info.sh
+++ bin/info.sh
@@ -3,8 +3,22 @@ str1="Images JPG:"
 str2="Images PNG:"
 str3="Free storage:"
 myAvai=$(df -h /tmp | tail -1 | awk '{print $4}')
+myIfy=$(find /Users/huyhoang8398/Desktop/scann -mtime -1 -ls)
+
+function GET_OUTPUT()
+{
+    echo -e "$str3 $myAvai\n";  
+}
+function OUTPUT_IFY()
+{
+    echo -e "$myIfy\n";
+}
+
 date > /Users/huyhoang8398/test.txt
 printf "`echo $str1` `find /Users/huyhoang8398/Pictures | grep .jpg | wc -l`\n" >> /Users/huyhoang8398/test.txt
 printf "`echo $str2` `find /Users/huyhoang8398/Pictures | grep .png | wc -l`\n" >> /Users/huyhoang8398/test.txt
-echo "$str3 $myAvai" >> /Users/huyhoang8398/test.txt
+echo $(GET_OUTPUT) >> /Users/huyhoang8398/test.txt
+echo $(OUTPUT_IFY) >> /Users/huyhoang8398/test.txt
+
+
 
